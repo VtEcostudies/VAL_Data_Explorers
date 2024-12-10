703274232030ab866a8f128d9e171d5c0a176c5d
jloomisVCE <jloomis@vtecostudies.org>
2024-12-10 14:19:19 -0500

    add freshworks widget to non-wp pages.

---
+++ js/freshworks.js
@@ -0,0 +1,3 @@
+window.fwSettings={'widget_id':62000000631};
+!function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}()
+await import('https://widget.freshworks.com/widgets/62000000631.js');
