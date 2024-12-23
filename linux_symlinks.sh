e69af2ea58ded125f4fd87576f691f8a4cd1c7a2
jloomisVCE <44261238+jloomisVCE@users.noreply.github.com>
2024-12-23 08:55:17 -0500

    symlink files

---
+++ linux_symlinks.sh
@@ -0,0 +1,2 @@
+ln -s ../VAL_Web_Utilities VAL_Web_Utilities
+ln -s ../VAL_Species_Page VAL_Species_Page
--- make_windows_links.bat
+++
@@ -1,2 +0,0 @@
-mklink /D VAL_Web_Utilities ..\VAL_Web_Utilities
-mklink /D VAL_Species_Page ..\VAL_Species_Page
