52046623b14fbc11789ae440adaea66ce01de566
jloomisVCE <jloomis@vtecostudies.org>
2025-05-20 07:31:40 -0400

    upgrade lit widget

--- js/gbif_lit_widget.js
+++ js/gbif_lit_widget.js
@@ -1,9 +1,7 @@
-/*
-REACT EVENT SEARCH SCRIPTS
-*/
 
 import { siteConfig } from './gbifSiteConfig.js'; //in html must declare this as module eg. <script type="module" src="js/gbif_data_config.js"></script>
 import { getStoredData } from '../../VAL_Web_Utilities/js/storedData.js';
+import { render } from "https://www-lib.gbif.org/gbif-lib.js";
 
 let siteName = siteConfig.siteName;
 let storSite = await getStoredData('siteName', '', '');
@@ -13,66 +11,59 @@ import(`../../VAL_Web_Utilities/js/gbifDataConfig.js?siteName=${siteName}`).then
   let dataConfig = fileConfig.dataConfig
 
   //console.log( `gbif_lit_widget publishingOrKey: ${dataConfig.publishingOrgKey}`);
-
-  var widgetLocale = 'en';
-
-  var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-    dense: true,
-    primary: '#176f75',
-    linkColor: '#176f75',
-    fontSize: '15px',
-    background: '#E7E7E7',
-    paperBackground: '#ffffff',
-    paperBorderColor: '#e0e0e0',
-    color: '#162d3d',
-    darkTheme: false,
-    fontFamily: '"Roboto", BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica", "Arial", sans-serif',
-    borderRadius: 4,
-    drawerZIndex: 50001
-  }});
-
-  var userTheme = typeof siteTheme !== 'undefined' ? siteTheme : undefined;
-  var userConfig = typeof siteConfig !== 'undefined' ? siteConfig : {};
-  var routes = userConfig.routes || {
-      literatureSearch: {
-      route: '/',
-    }
-  };
-
+/*
   //sometimes the widget inserts basename into path (eg. on page-reload). detect host and use relevant path.
   if ('vtatlasoflife.org' == dataConfig.hostUrl || 'localhost' == dataConfig.hostUrl.split(':')[0]) {
     routes.basename = '/';
   } else {
     routes.basename = '/gbif-literature';
   }
-
-  //VCE publishingOrganizationKey=b6d09100-919d-4026-b35b-22be3dae7156
-
-  /*
-  predicate: {
-    type: 'or', predicates: [
-      {
-        type: 'in',
-        key: 'countriesOfResearcher',
-        values: ['US', 'VI']
-      },
-      {
-        type: 'in',
-        key: 'countriesOfCoverage',
-        values: ['US']
-      }
-    ]
-  }
-  */
-
-  var literature = {
-      rootFilter: {
-        predicate: {
-          type: 'or', predicates: [
+*/
+  render(document.getElementById("root"), {
+    "version": 3,
+    "pages": [
+        {
+            "id": "literatureSearch"
+            ,"path": window.location.pathname
+        }
+    ],
+    "disableInlineTableFilterButtons": false,
+    "availableCatalogues": [],
+    "dataHeader": {
+        "enableApiPopup": false,
+        "enableInfoPopup": false
+    },
+    "theme": {
+        "primary": "#176f75",
+        "borderRadius": 3,
+        "stickyOffset": "0px"
+        },
+    "languages": [
+        {
+            "code": "en",
+            "localeCode": "en",
+            "label": "English",
+            "default": true,
+            "textDirection": "ltr",
+            "iso3LetterCode": "eng",
+            "cmsLocale": "en-GB",
+            "gbifOrgLocalePrefix": ""
+        }
+      ],
+    "messages": {},
+    "occurrenceSearch": {},
+    "collectionSearch": {},
+    "institutionSearch": {},
+    "datasetSearch": {},
+    "publisherSearch": {},
+    "literatureSearch": {
+        "scope": {
+          "type": 'or', 
+          "predicates": [
             {
-              type: 'in',
-              key: 'publishingOrganizationKey',
-              values: [dataConfig.publishingOrgKey]
+              "type": 'in',
+              "key": 'publishingOrganizationKey',
+              "values": [dataConfig.publishingOrgKey]
             },
 /*
             {
@@ -82,25 +73,15 @@ import(`../../VAL_Web_Utilities/js/gbifDataConfig.js?siteName=${siteName}`).then
             }
 */
           ]
-        }
-      },
-      //highlightedFilters: ['q', 'countriesOfResearcher', 'countriesOfCoverage', 'year']
-      highlightedFilters: ['q', 'dataSet', 'year'],
-      excludedFilters: ['countriesOfCoverage']
-    }
-
-  ReactDOM.render(
-    React.createElement(gbifReactComponents.LiteratureSearch, {
-      style: { 'min-height': 'calc(100vh - 80px)' },
-      siteConfig: {
-        theme: userTheme,
-        routes: routes,
-        locale: widgetLocale,
-        literature: literature
-      },
-      pageLayout: true,
-    }),
-    document.getElementById('root')
-  );
-
+        },
+      "highlightedFilters": [
+          "q",
+          "dataSet",
+          "year"
+        ],
+      "excludedFilters": [
+        "countriesOfCoverage"
+        ],
+      }
+    })
 })
