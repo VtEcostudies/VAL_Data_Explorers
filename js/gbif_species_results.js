97869650d48193a44a801956b58b7005db003679
jloomisVCE <jloomis@vtecostudies.org>
2025-10-17 20:09:51 -0400

    another try at parentUrl

--- js/gbif_species_results.js
+++ js/gbif_species_results.js
@@ -860,9 +860,9 @@ async function startUp(fCfg) {
   }
 
   if (embed && fCfg.hostConfig.parentUrl) {
-    exploreUrl = fCfg.hostConfig.parentUrl.href + fCfg.hostConfig.explorePath;
-    resultsUrl = fCfg.hostConfig.parentUrl.href + fCfg.hostConfig.resultsPath;
-    profileUrl = fCfg.hostConfig.parentUrl.href + fCfg.hostConfig.profilePath;
+    exploreUrl = fCfg.hostConfig.parentUrl.href + fCfg.hostConfig.baseRoute + fCfg.hostConfig.wpRoute.occurrences;
+    resultsUrl = fCfg.hostConfig.parentUrl.href + fCfg.hostConfig.resultsPath + fCfg.hostConfig.wpRoute.speciesList;
+    profileUrl = fCfg.hostConfig.parentUrl.href + fCfg.hostConfig.profilePath + fCfg.hostConfig.wpRoute.speciesProfile;
   } else {
     homeUrl = fCfg.dataConfig.homeUrl;
     exploreUrl = fCfg.dataConfig.exploreUrl;
